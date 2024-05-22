89033c8b42779e479d7295d3e9c74be2a62a4bb9
Prashuk Jain <jainprashuk5012258@gmail.com>
2024-05-22 17:57:15 +0530

    third commit

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import views
+from .views import fun
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.fun),
+    path('',fun, name='fun'),
 ]
--- backend/core/core/views.py
+++ backend/core/core/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse,HttpRequest
 from django.shortcuts import render 
 def fun(request):
-    return render(request, 'chatbot.html') 
+    if request.method == 'POST':
+        form_data = request.POST.get('input_name')
+        context = {
+            'form_data' :form_data
+        }
+        return render(request, 'chatbot.html', context) 
+    else:
+        return render(request, 'chatbot.html') 
