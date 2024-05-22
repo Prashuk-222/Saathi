6f3215b72a50005b6ad87aebd04ae29701e0e72b
Prashuk Jain <jainprashuk5012258@gmail.com>
2024-05-22 16:57:17 +0530

    third commit

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +32,7 @@ ALLOWED_HOSTS = []
 
 # Static files directory 
 STATICFILES_DIRS = [
-    'frontend/',
+    os.path.join(BASE_DIR, 'frontend/static'),
 ]
 
 
@@ -60,7 +62,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['frontend/'],
+        'DIRS': [
+                os.path.join(BASE_DIR, 'frontend/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'frontend/'
+STATIC_URL = '/frontend/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/core/core/views.py
+++ backend/core/core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse,HttpRequest
 from django.shortcuts import render 
 def fun(request):
-    return render(request, "chatbot.html") 
+    return render(request, 'chatbot.html') 
